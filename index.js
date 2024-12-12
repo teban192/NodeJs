2a032c2aaa519884ca7523a19913ef411170611a
teban192 <mmonestelalf@gmail.com>
2024-12-12 12:11:42 -0600

    Asynchronous way to read file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
+//Blocking synchronous way
 
-const testIn=fs.readFileSync('./txt/example.txt','utf-8');
-console.log(testIn);
+//const testIn=fs.readFileSync('./txt/example.txt','utf-8');
+//console.log(testIn);
 
-const testOut = `This is what we know about the avocado: ${testIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt',testOut);
+//const testOut = `This is what we know about the avocado: ${testIn}.\nCreated on ${Date.now()}`;
+//fs.writeFileSync('./txt/output.txt',testOut);
 
-console.log('File written!');
+//console.log('File written!');
+
+//Non-blocking ,  asynchronous way
+fs.readFile('./txt/starts.txt','utf-8',(err,data) =>{
+ console.log(data);
+});
+
+console.log('Will read file!');
+
